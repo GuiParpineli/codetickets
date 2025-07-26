455dc656763c4c47223caa665346329d1ec8e544
Gui Parpineli <guiparpineli@hotmail.com>
2025-07-26 12:38:55 -0300

    Add ImportationController and update data handling files
    - Introduced `ImportationController` to handle file import API endpoint.
    - Updated `dados.csv` with new ticket data for 2024.
    - Added Spring Web dependency for REST support in `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.batch:spring-batch-test")
